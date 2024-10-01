59c354f5479b5c0489ce3397031b94cae98bc477
ItsRohit007 <gajananraut852@gmail.com>
2024-10-02 01:58:30 +0530

    first commit

---
+++ src/components/banner/LeftBanner.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import {
+  // FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaReact,
+  FaGithub,
+  FaInstagram,
+  FaAndroid,
+} from "react-icons/fa";
+import {  SiNextdotjs, SiKotlin } from "react-icons/si";
+
+const LeftBanner = () => {
+  const [text] = useTypewriter({
+    words: ["Mobile Developer.", "Web Developer.", "UI Designer."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Rohit Raut </span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+        Motivated Computer Science Engineering student with a strong passion for Android and Web development. Proficient in Java, Kotlin, Python, and UI/UX design, with a focus on building user-friendly mobile applications. Seeking opportunities to apply my skills and contribute to innovative projects at leading tech companies.
+        </p>
+      </div>
+      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <a href="https://github.com/rohitRaut007" target="blank">
+              <span className="bannerIcon">
+                <FaGithub />
+              </span>
+            </a>
+            <a href="https://www.instagram.com/raut.rohit_/" target="blank">
+              <span className="bannerIcon">
+                <FaInstagram />
+              </span>
+            </a>
+            <a href="https://github.com/noorjsdivs/portfolioone" target="blank">
+              <span className="bannerIcon">
+                <FaTwitter />
+              </span>
+            </a>
+            <a href="https://www.linkedin.com/in/rohit-raut-91369a27a/" target="blank">
+              <span className="bannerIcon">
+                <FaLinkedinIn />
+              </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaAndroid />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiKotlin />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftBanner;
